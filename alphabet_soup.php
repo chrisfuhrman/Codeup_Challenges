2c002fa99319792fc6a33f6966aac70c3eb2d109
Chris Fuhrman <chrisfuhrman@gmail.com>
2014-11-29 15:08:21 -0600

    updated comments

--- alphabet_soup.php
+++ alphabet_soup.php
@@ -13,17 +13,19 @@ function alphabet_soup($str) {
 	$str = strtolower($str);
 	//removes special characters but not spaces
 	$str = preg_replace("/[^ \w]+/", '', $str);
-	// // turn string into array, with each word as a separate element
+	// turns $str into an array, with each word as a separate element
 	$array = str_split($str);
+	//Alphabetizes the string
 	sort($array);
-	echo implode($array) . PHP_EOL;
+	//convert array to string
+	return implode($array) . PHP_EOL;
 }
 
 // ask user for input
 fwrite(STDOUT, "enter a word or phrase \n");
 
 // call function using user input
-alphabet_soup(trim(fgets(STDIN)));
+echo alphabet_soup(trim(fgets(STDIN)));
 
 
 
