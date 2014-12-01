aaffa14a39899dcafde1b0e76bd4a62de775da83
Chris Fuhrman <chrisfuhrman@gmail.com>
2014-12-01 08:53:00 -0600

    corrected to work for more than one word

--- alphabet_soup.php
+++ alphabet_soup.php
@@ -7,31 +7,31 @@
 // E.g. "hello world" becomes "ehllo dlorw". So make sure your 
 // function separates and alphabetizes each word separately.
 
-
 function alphabet_soup($str) {
 	//lowercase input
 	$str = strtolower($str);
 	//removes special characters but not spaces
 	$str = preg_replace("/[^ \w]+/", '', $str);
+	// converts $str into an array with each word as an element
+	$array1 = explode(' ', $str);
 	// turns $str into an array, with each word as a separate element
-	$array = str_split($str);
-	//Alphabetizes the string
-	sort($array);
-	//convert array to string
-	return implode($array) . PHP_EOL;
+	
+//converts each element of $array1 their own array,
+//then alphabetizes each array and returns the value as a string
+	foreach ($array1 as $value) {
+		// cycles through $array1 and breaks each element into its own string
+		$array2 = str_split($value);
+		//Alphabetizes each word separately in the string
+		sort($array2);
+		// echos the value from the array and puts a space between each word
+		echo implode($array2) . ' ';
+	}
 }
 
 // ask user for input
 fwrite(STDOUT, "enter a word or phrase \n");
 
 // call function using user input
-echo alphabet_soup(trim(fgets(STDIN)));
-
-
-
-
-
-
-
-
+$str = (trim(fgets(STDIN)));
+echo alphabet_soup($str) . PHP_EOL;
 
